bca053f89f87934930b8ee340de0d61e9fe47c6b
Max Stankevich <max.stankevich.me@gmail.com>
2019-10-10 04:01:22 +0300

    chore: jsdoc for EnhanceType

--- packages/react-core/src/util/enhance-type.ts
+++ packages/react-core/src/util/enhance-type.ts
@@ -1,5 +1,10 @@
 import { ComponentType } from "react";
 
+/**
+ * Component type which is result of the HOC function.
+ * @see https://reactjs.org/docs/higher-order-components.html
+ */
 export type EnhanceType<P = any> = ComponentType<P> & {
+  /** Source component being enhanced by the HOC. */
   source?: ComponentType<P>;
 };
