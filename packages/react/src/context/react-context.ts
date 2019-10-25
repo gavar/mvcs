ba02f4a8b35eb0218e26db25d5d674ed5f7de840
gavar <max.stankevich.me@gmail.com>
2019-10-25 16:52:06 +0300

    fix: allow to use mvcs without root context provider

--- packages/react/src/context/react-context.ts
+++ packages/react/src/context/react-context.ts
@@ -1,4 +1,11 @@
 import { Context, DefaultContext } from "@mvcs/context";
 import { createContext } from "react";
 
-export const ReactContext = createContext<Context>(new DefaultContext());
+export const RootContext = factory();
+export const ReactContext = createContext(RootContext);
+
+function factory(): Context {
+  const context = new DefaultContext();
+  context.initialize();
+  return context;
+}
